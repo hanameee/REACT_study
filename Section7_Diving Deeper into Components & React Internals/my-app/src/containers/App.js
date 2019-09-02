48d34f50e54a7bee450bffab19ff96da72225c33
hanameee <hanameee@likelion.org>
2019-09-02 18:19:40 +0900

    component update lifecycle for state

--- Section7_Diving Deeper into Components & React Internals/my-app/src/containers/App.js
+++ Section7_Diving Deeper into Components & React Internals/my-app/src/containers/App.js
@@ -29,6 +29,15 @@ class App extends Component {
         console.log("[App.js] componentDidMount");
     }
 
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("[App.js] shouldComponentUpdate");
+        return true;
+    }
+
+    componentDidUpdate() {
+        console.log("[App.js] componentDidUpdate");
+    }
+
     deletePersonHandler = personIndex => {
         const persons = [...this.state.persons];
         persons.splice(personIndex, 1);
