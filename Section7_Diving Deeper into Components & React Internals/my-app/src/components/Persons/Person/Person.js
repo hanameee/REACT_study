e7bd57c0f6cde7ebdf10d9216c36b411ac545f79
hanameee <hanameee@likelion.org>
2019-09-02 16:46:44 +0900

    component update lifecycle for props

--- Section7_Diving Deeper into Components & React Internals/my-app/src/components/Persons/Person/Person.js
+++ Section7_Diving Deeper into Components & React Internals/my-app/src/components/Persons/Person/Person.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./Person.module.css";
 
 //우리는 component를 extends 한 class를 만드는게 아니라 그냥 function을 만들거라서 App.js 에서처럼 component를 Import 해올 필요가 없음
 
 //props는 제한되지 않은 숫자의 arguments 를 받을 수 있다.
 
-const person = props => {
-    console.log("[Person.js]] rendering");
-    return (
-        <div className = {styles.Person}>
-            <p onClick={props.click}>
-                I'm {props.name} and I am {props.age} years old!
-            </p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-            {/* <input type="text" onChange={props.changed}/> */}
-        </div>
-    );
-};
+class Person extends Component {
 
-export default person;
+    render() {
+        console.log("[Person.js]] rendering");
+        return (
+            <div className = {styles.Person}>
+                <p onClick={this.props.click}>
+                    I'm {this.props.name} and I am {this.props.age} years old!
+                </p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                {/* <input type="text" onChange={props.changed}/> */}
+            </div>
+        );
+    }
+}
+// const person = props => {
+//     console.log("[Person.js]] rendering");
+//     return (
+//         <div className = {styles.Person}>
+//             <p onClick={props.click}>
+//                 I'm {props.name} and I am {props.age} years old!
+//             </p>
+//             <p>{props.children}</p>
+//             <input type="text" onChange={props.changed} value={props.name} />
+//             {/* <input type="text" onChange={props.changed}/> */}
+//         </div>
+//     );
+// };
+
+// export default person;
+export default Person;
--- Section7_Diving Deeper into Components & React Internals/my-app/src/components/Persons/Persons.js
+++ Section7_Diving Deeper into Components & React Internals/my-app/src/components/Persons/Persons.js
@@ -1,19 +1,57 @@
-import React from 'react';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import Person from "./Person/Person";
 
-const persons = (props) => {
-    console.log("[Persons.js]] rendering");
-    return props.persons.map((person,index) => {
-        return(
-            <Person
-                click = {() => props.clicked(index)}
-                name = {person.name}
-                age = {person.age}
-                changed = {event => props.changed(event, person.id)}
-                key = {person.id}
-            />
-        );
-    });
-};
+class Persons extends Component {
 
-export default persons;
+    // static getDerivedStateFromProps(props,state){
+    //     console.log("[Person.js] getDerivedStateFromProps");
+    //     return state;
+    // }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("[Persons.js] shouldComponentUpdate");
+        return true; //true if
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log("[Persons.js] getSnapshotBeforeUpdate");
+        return { message : "snapshot!" };
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("[Persons.js] componentDidUpdate");
+        console.log(snapshot)
+    }
+
+    render() {
+        console.log("[Persons.js]] rendering");
+        return this.props.persons.map((person, index) => {
+            return (
+                <Person
+                    click={() => this.props.clicked(index)}
+                    name={person.name}
+                    age={person.age}
+                    changed={event => this.props.changed(event, person.id)}
+                    key={person.id}
+                />
+            );
+        });
+    }
+}
+// const persons = (props) => {
+//     console.log("[Persons.js]] rendering");
+//     return props.persons.map((person,index) => {
+//         return(
+//             <Person
+//                 click = {() => props.clicked(index)}
+//                 name = {person.name}
+//                 age = {person.age}
+//                 changed = {event => props.changed(event, person.id)}
+//                 key = {person.id}
+//             />
+//         );
+//     });
+// };
+
+// export default persons;
+export default Persons;
