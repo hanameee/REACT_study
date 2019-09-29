6d80a1e19b1bd26fcda880ed393e18e8ae16e954
hanameee <hanameee@likelion.org>
2019-09-29 19:09:25 +0900

    Modified SideDrawer component

--- Section8_The Burger Builder (Basic Version)/burger-builder/src/components/Layout/Layout.js
+++ Section8_The Burger Builder (Basic Version)/burger-builder/src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React , { Fragment } from "react";
 import styles from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 const layout = ( props ) => (
     <Fragment>
-        <Toolbar/>
+        <Toolbar />
+        <SideDrawer />
         <main className = {styles.Content}>
             {props.children}
         </main>
