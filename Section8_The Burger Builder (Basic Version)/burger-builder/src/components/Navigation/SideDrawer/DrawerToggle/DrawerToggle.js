b24c58ec63740883f6909681c6db53319ffc1774
hanameee <hanameee@likelion.org>
2019-09-30 15:53:50 +0900

    Added hamburger icon styling to toggle button

--- Section8_The Burger Builder (Basic Version)/burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ Section8_The Burger Builder (Basic Version)/burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import styles from "./DrawerToggle.module.css";
 
-const drawerToggle = (props) => (
-    <div onClick = {props.clicked}>MENU</div>
-)
+const drawerToggle = props => (
+    <div className = {styles.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div className = {styles.Last}></div>
+    </div>
+);
 
 export default drawerToggle;
