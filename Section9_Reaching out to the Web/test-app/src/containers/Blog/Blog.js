23666485e07412b0f87c56e97cdc598875b1d6d7
hanameee <hanameee@likelion.org>
2019-10-02 17:20:43 +0900

    Transformed data (added Auther, sliced data)

--- Section9_Reaching out to the Web/test-app/src/containers/Blog/Blog.js
+++ Section9_Reaching out to the Web/test-app/src/containers/Blog/Blog.js
@@ -6,19 +6,33 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(response => {
+            const posts = response.data.slice(0,4);
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Hannah'
+                }
+            });
+            this.setState(
+                {posts : updatedPosts}
+            )
             console.log(response);
         });
     }
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post title = {post.title} author = {post.author} key = {post.id}/>
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
