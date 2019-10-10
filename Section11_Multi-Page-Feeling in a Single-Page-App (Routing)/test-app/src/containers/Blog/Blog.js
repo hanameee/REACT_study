9a50308a6b13f351add5adb974dbc293924954e5
hanameee <hanameee@likelion.org>
2019-10-10 19:12:09 +0900

    Render component for specific path using Route

--- Section11_Multi-Page-Feeling in a Single-Page-App (Routing)/test-app/src/containers/Blog/Blog.js
+++ Section11_Multi-Page-Feeling in a Single-Page-App (Routing)/test-app/src/containers/Blog/Blog.js
@@ -1,54 +1,14 @@
 import React, { Component } from "react";
 // import axios from "axios";
-import axios from '../../axios'
-
+import axios from '../../axios';
+import Posts from './Posts/Posts';
+import { Route } from 'react-router-dom';
 import "./Blog.css";
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        errorState: false
-    };
 
-    componentDidMount() {
-        axios
-            .get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: "Hannah"
-                    };
-                });
-                this.setState({ posts: updatedPosts });
-            })
-            .catch(error => {
-                this.setState({
-                    errorState: true
-                })
-            });
-    };
-
-    postSelectedHandler = (id) => {
-        this.setState({
-            selectedPostId : id
-        })
-    };
-   
     render() {
-        let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>;
-        // if(!this.state.errorState){
-        //     posts = this.state.posts.map(post => {
-        //         return (
-        //             <Post title={post.title}
-        //                 author={post.author} 
-        //                 key={post.id} 
-        //                 clicked = {() => this.postSelectedHandler(post.id)}/>
-        //         );
-        //     });
-        // }
+
         return (
             <div className = 'Blog'>
                 <header>
@@ -59,7 +19,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">{posts}</section>
+                <Route path = '/' exact component = {Posts}/>
                 {/* <section>
                     <FullPost id = {this.state.selectedPostId}/>
                 </section>
