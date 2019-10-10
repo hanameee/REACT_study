1128f2510c16bd90b9c69a930daa440edb117479
hanameee <hanameee@likelion.org>
2019-10-10 15:50:18 +0900

    Change Fullpost and Newpost to container and install react-route & react-route-dom

--- Section11_Multi-Page-Feeling in a Single-Page-App (Routing)/test-app/src/containers/Blog/Blog.js
+++ Section11_Multi-Page-Feeling in a Single-Page-App (Routing)/test-app/src/containers/Blog/Blog.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 // import axios from "axios";
 import axios from '../../axios'
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
+
 import "./Blog.css";
 
 class Blog extends Component {
@@ -38,28 +36,36 @@ class Blog extends Component {
             selectedPostId : id
         })
     };
-        
+   
     render() {
         let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>;
-        if(!this.state.errorState){
-            posts = this.state.posts.map(post => {
-                return (
-                    <Post title={post.title}
-                        author={post.author} 
-                        key={post.id} 
-                        clicked = {() => this.postSelectedHandler(post.id)}/>
-                );
-            });
-        }
+        // if(!this.state.errorState){
+        //     posts = this.state.posts.map(post => {
+        //         return (
+        //             <Post title={post.title}
+        //                 author={post.author} 
+        //                 key={post.id} 
+        //                 clicked = {() => this.postSelectedHandler(post.id)}/>
+        //         );
+        //     });
+        // }
         return (
-            <div>
+            <div className = 'Blog'>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href = '/'>Home</a></li>
+                            <li><a href = '/new-post'>New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">{posts}</section>
-                <section>
+                {/* <section>
                     <FullPost id = {this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
-                </section>
+                </section> */}
             </div>
         );
     }
